19ed29682bc5c40a01e881f049e942a728eb5c19
Lara Grenzendorf <grenzendorflara@gmail.com>
2024-07-31 15:01:28 +0100

    add and validate user input

--- run.py
+++ run.py
@@ -13,8 +13,37 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 
-sales = SHEET.worksheet('sales')
+# sales = SHEET.worksheet('sales')
 
-data = sales.get_all_values()
+# data = sales.get_all_values()
 
-print(data)
+# print(data)
+
+def get_sales_data():
+
+    """
+    Get sales figures input from user
+    """
+
+    print("Please enter sales data from the last market day")
+    print('Daa should be in the format of six numbers, separated by commas')
+    print('Example: 0, 10, 20, 30, 40, 50\n')
+
+    data_str = input('Enter your data here: ')
+    
+    sales_data = data_str.split(',')
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Attempts to convert string vslues to int, raises ValueError if not possible or there are not exactly 6 values
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(f'6 Values required, user provided {len(values)}')
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again\n")
+
+get_sales_data()
