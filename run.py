8318bf6e5d64ca1d752744790d486b8371e34622
Lara Grenzendorf <grenzendorflara@gmail.com>
2024-07-31 23:40:39 +0100

    update stock worksheet and refactor code

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -57,17 +58,49 @@ def validate_data(values):
 
     return True
 
-def update_sales_worksheet(data):
+
+def update_worksheet(data, worksheet):
     """
-    Update sales worksheet, add new row with the list data from user
+    Updates specified worksheet with data provided
     """
 
-    print('Updating sales worksheet\n')
-    sales_worksheet = SHEET.worksheet('sales')
-    sales_worksheet.append_row(data)
-    print('Sales worksheet updated \n')
+    print(f'Updating {worksheet} worksheet \n')
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f'{worksheet} worksheet updated \n')
 
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate surplus
+    - positive surplus indicates waste
+    - negative surplus indicates extra made on demand when stock ran out
+    """
+
+    print('Calculating surplus data \n')
+    stock = SHEET.worksheet('stock').get_all_values()
+    stock_row = stock[-1]
+    stock_row = [int(x) for x in stock_row]
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = stock - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
+
+
+
+def main():
+    """
+    Run main program functions
+    """
+
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_worksheet(data, 'sales')
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_worksheet(new_surplus_data, 'surplus')
+
+print('Welcome to Love Sandwiches Data Automation')
+main()
